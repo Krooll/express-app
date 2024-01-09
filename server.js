f4c861666bca4145425e97418861fb477ae0788f
Pawel Krol <paullkroll@icloud.com>
2024-01-09 23:55:37 +0100

    Add multer

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
+const multer = require('multer');
 
 const app = express();
 
@@ -9,15 +10,19 @@ app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 
 app.set('view engine', '.hbs');
 app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'dark' }));
 
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage })
+
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.post('/contact/send-message', (req, res) => {
 
-  const { author, sender, title, message, file } = req.body;
+  const { author, sender, title, message } = req.body;
 
-  if(author && sender && title && message && file) {
-    res.render('contact', { isSent: true, layout: 'main' });
+  if(author && sender && title && message && req.file) {
+    res.render('contact', { isSent: true, name: req.file.originalname, layout: 'main' });
   }
   else {
     res.render('contact', { isError: true, layout: 'main' });
