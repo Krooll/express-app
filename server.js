42f05f731fd5be477cb30fe8ce2b4d32b48952ea
Pawel Krol <paullkroll@icloud.com>
2024-01-09 16:31:29 +0100

    Add hbs file

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
+
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
 
 app.use((req, res, next) => {
   res.show = (name) => {
@@ -11,6 +15,10 @@ app.use((req, res, next) => {
 });
 
 app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { layout: false, name: req.params.name });
+});
 
 app.get('/', (req, res) => {
   res.show('index.html');
@@ -34,7 +42,7 @@ app.get('/history', (req, res) => {
 
 app.use((req, res) => {
     res.status(404).send('404 not found...');
-  })
+});
 
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
