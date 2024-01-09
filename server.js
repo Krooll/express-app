1fcad820d69a4ae39af449f7fdb353749a49cb89
Pawel Krol <paullkroll@icloud.com>
2024-01-09 18:50:08 +0100

    End work module 27.3 - all works

--- server.js
+++ server.js
@@ -4,40 +4,35 @@ const hbs = require('express-handlebars');
 
 const app = express();
 
-app.engine('.hbs', hbs());
-app.set('view engine', '.hbs');
+app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
+app.set('view engine', '.hbs');
+app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'dark' }));
 
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/hello/:name', (req, res) => {
-  res.render('hello', { layout: false, name: req.params.name });
+  res.render('hello', { name: req.params.name });
 });
 
 app.get('/', (req, res) => {
-  res.show('index.html');
+  res.render('index', { layout: 'main' })
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about', { layout: 'dark' })
 });
 
 app.get('/contact', (req, res) => {
-  res.show('contact.html');
+  res.render('contact', { layout: 'main' })
 });
 
 app.get('/info', (req, res) => {
-  res.show('info.html');
+  res.render('info', { layout: 'dark' })
 });
 
 app.get('/history', (req, res) => {
-  res.show('history.html');
+  res.render('history', { layout: 'main' })
 });
 
 app.use((req, res) => {
